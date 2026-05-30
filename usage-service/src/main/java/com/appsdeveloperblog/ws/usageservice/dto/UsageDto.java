33fdf9a7ddca095727ebb59975a697e1e557377e
José Vitor <jose.vitorw018@gmail.com>
2026-05-29 22:37:03 -0300

    Ajustes UsageDTO

--- usage-service/src/main/java/com/appsdeveloperblog/ws/usageservice/dto/UsageDto.java
+++ usage-service/src/main/java/com/appsdeveloperblog/ws/usageservice/dto/UsageDto.java
@@ -2,13 +2,11 @@ package com.appsdeveloperblog.ws.usageservice.dto;
 
 import lombok.Builder;
 
+import java.util.List;
+
 @Builder
 public record UsageDto(
-        Long id,
-        String name,
-        String type,
-        String location,
         Long userId,
-        Double energyConsumed
-) {
+        List<DeviceDto> devices
+)  {
 }
